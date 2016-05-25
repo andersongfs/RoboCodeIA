786c91a4c32d0c90d7036cae12e0fdad5976a909
tiaraju <tiarajums@gmail.com>
2016-05-25 17:28:33 -0300

    some changes

--- src/genetictank/GeneticAlgorithm.java
+++ src/genetictank/GeneticAlgorithm.java
@@ -9,66 +9,53 @@ public class GeneticAlgorithm{
 	
 	private static int POPULATION_SIZE = 20;
 	private static int RENEW_RATE = 15;
-	private static int MAX_ROUNDS = 5;
-	
+	private static int MAX_NUMBER_OF_ROUNDS = 5;
 	private static int MUTATION_RATIO = 20; 
 	private static int MAX_GENES = 6;
-	
 	private static int MIN_BULLET_POWER = 1;
 	private static int MAX_BULLET_POWER = 3;
 	private static int BULLET_POWER_PRECISION = 10;
-	
 	private static List<Chromosome> chromosomes;
-	
-	private static List<Double> relativeStatistics;
-	private static List<Double> absoluteStatistics;
-	
-	private static int pos;
+	private static List<Double> partilStatistics;
+	private static List<Double> totalStatistics;
+	private static int initialPosition;
 	private static int round;
-	
-	private static int absoluteWins;
-	private static int absoluteRounds;
-	private static int relativeWins;
-	private static int relativeRounds;
+	private static int totalNumberOfWins;
+	private static int totalNumberOfRounds;
+	private static int partialNumberOfWins;
+	private static int partialNumberOfRounds;
 	
 	public static void start(GeneticRobot robot) {
-		
 		if (chromosomes == null) {
-			chromosomes = new ArrayList<Chromosome>();
-			
-			for (int i = 0; i < POPULATION_SIZE; i++)
-				chromosomes.add(generateRandomChromosome(i));
-			
-			pos = 0;
+			generateRandomPopulation();			
+			initialPosition = 0;
+			totalNumberOfRounds = 0;
+			totalNumberOfWins = 0;
+			partialNumberOfRounds = 0;
+			partialNumberOfWins = 0;
 			round = 0;
-			absoluteWins = 0;
-			absoluteRounds = 0;
-			relativeWins = 0;
-			relativeRounds = 0;
-			
-			relativeStatistics = new ArrayList<Double>();
-			absoluteStatistics = new ArrayList<Double>();
+			totalStatistics = new ArrayList<Double>();
+			partilStatistics = new ArrayList<Double>();
 		}
-		
-		if (round == MAX_ROUNDS) {
-			pos++;
+		if (round == MAX_NUMBER_OF_ROUNDS) {
+			initialPosition++;
 			round = 0;
 		}
-		
-		if (pos == POPULATION_SIZE) {
-			pos = 0;
+		if (initialPosition == POPULATION_SIZE) {
+			initialPosition = 0;
 			round = 0;
 			renewGeneration();
-			relativeStatistics.add((((double)relativeWins)/relativeRounds)*100);
-			absoluteStatistics.add((((double)absoluteWins)/absoluteRounds)*100);
-			relativeWins = 0;
-			relativeRounds = 0;
+			partilStatistics.add((((double)partialNumberOfWins)/partialNumberOfRounds)*100);
+			totalStatistics.add((((double)totalNumberOfWins)/totalNumberOfRounds)*100);
+			partialNumberOfWins = 0;
+			partialNumberOfRounds = 0;
 		}
-		printStatistics();
-		System.out.println("Going with robot " + pos + " of " + chromosomes.size() + " in round " + round);
-		robot.chromosome = chromosomes.get(pos);
+		showResults();
+		System.out.println("The battle has started with the robot of number" + initialPosition + " out of " + chromosomes.size() + "! Round number: " + round);
+		robot.chromosome = chromosomes.get(initialPosition);
 	}
 	
+
 	private static Chromosome generateRandomChromosome(int id) {
 		Chromosome chromosome = new Chromosome(id);
 		
@@ -92,28 +79,33 @@ public class GeneticAlgorithm{
 		return chromosome;
 	}
 	
+	private static void generateRandomPopulation() {
+		chromosomes = new ArrayList<Chromosome>();		
+		for (int i = 0; i < POPULATION_SIZE; i++)
+			chromosomes.add(generateRandomChromosome(i));
+	}
+
+	
 	public static void onRobotWin(Chromosome chromosome) {
 		chromosome.increaseFitness();
-		absoluteWins++;
-		relativeWins++;
+		totalNumberOfWins++;
+		partialNumberOfWins++;
 	}
 	
 	public static void onRoundEnded(Chromosome chromosome) {
 		round++;
-		absoluteRounds++;
-		relativeRounds++;
+		totalNumberOfRounds++;
+		partialNumberOfRounds++;
 	}
 	
 	public static void onBattleEnded(Chromosome chromosome) {
 		System.out.println("========== Relative Statistics ==========");
-		for(int i = 0; i < relativeStatistics.size(); i++) {
-			//System.out.println("Iteration " + i + ": " + relativeStatistics.get(i) + "%.");
-			System.out.println(relativeStatistics.get(i));
+		for(int i = 0; i < partilStatistics.size(); i++) {
+			System.out.println(partilStatistics.get(i));
 		}
 		System.out.println("========== Absolute Statistics ==========");
-		for(int i = 0; i < absoluteStatistics.size(); i++) {
-			//System.out.println("Iteration " + i + ": " + absoluteStatistics.get(i) + "%.");
-			System.out.println(absoluteStatistics.get(i));
+		for(int i = 0; i < totalStatistics.size(); i++) {
+			System.out.println(totalStatistics.get(i));
 		}
 	}
 	
@@ -137,7 +129,6 @@ public class GeneticAlgorithm{
 			int splitPoint = randomInt((geneSize+1)/2, Math.min(geneSize, MAX_GENES/2));
 			
 			crossOver(geneSize,splitPoint,parent1,parent2,chromosome);
-						
 		}
 		
 		for (int i = 0; i < POPULATION_SIZE; i++) {
@@ -148,7 +139,6 @@ public class GeneticAlgorithm{
 	private static void crossOver(int geneSize, int splitPoint,int parent1, int parent2,Chromosome chromosome){
 		List<Double> moveRotation = new ArrayList<Double>();
 		List<Double> moveDistance = new ArrayList<Double>();
-		
 		for(int j = 0; j < splitPoint; j++) {
 			double mr = chromosomes.get(parent1).getRotations().get(j);
 			double md = chromosomes.get(parent1).getDistances().get(j);
@@ -158,7 +148,6 @@ public class GeneticAlgorithm{
 				mr += randomInt(-45,45);
 				md += randomInt(-100,100);
 			}
-			
 			moveRotation.add(mr);
 			moveDistance.add(md);
 		}
@@ -182,24 +171,21 @@ public class GeneticAlgorithm{
 		
 		chromosome.setRotations(moveRotation);
 		chromosome.setDistances(moveDistance);
-		
 		double totalBulletPower = chromosomes.get(parent1).getBulletPower() +
 							chromosomes.get(parent2).getBulletPower();
-		
 		chromosome.setBulletPower(totalBulletPower / 2);
-		
 	}
 	
-	private static void printStatistics() {
-		if(relativeRounds > 0)
-			System.out.println("Relative Win ratio: " + ((((double)relativeWins)/relativeRounds)*100) + "%");
-		if(absoluteRounds > 0)
-			System.out.println("Absolute Win ratio: " + ((((double)absoluteWins)/absoluteRounds)*100) + "%");
+	private static void showResults() {
+		if(partialNumberOfRounds > 0)
+			System.out.println("Relative Win: " + ((((double)partialNumberOfWins)/partialNumberOfRounds)*100) + "%");
+		if(totalNumberOfRounds > 0)
+			System.out.println("Absolute Win: " + ((((double)totalNumberOfWins)/totalNumberOfRounds)*100) + "%");
 		
-		System.out.println("relativeWins: " + relativeWins);
-		System.out.println("relativeRounds: " + relativeRounds);
-		System.out.println("absoluteWins: " + absoluteWins);
-		System.out.println("absoluteRounds: " + absoluteRounds);
+		System.out.println("relativeRounds: " + partialNumberOfRounds);
+		System.out.println("relativeWins: " + partialNumberOfWins);
+		System.out.println("absoluteRounds: " + totalNumberOfRounds);
+		System.out.println("absoluteWins: " + totalNumberOfWins);
 	}
 	
 	private static int randomInt(int start, int end) {
